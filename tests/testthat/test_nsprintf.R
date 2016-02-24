688817c0d80b7188e8d5b739f33122837da3737b
Richard Cotton <rjc2003@qatar-med.cornell.edu>
2016-02-24 14:06:32 +0300

    updated tests

--- tests/testthat/test_nsprintf.R
+++ tests/testthat/test_nsprintf.R
@@ -1,8 +1,25 @@
 context("Test nsprintf")
 
-test_that("everything workds", {
-    n1 <- nsprintf(1, "there is one apples", "there are %d apples")
+test_that("simple pluralisation works", {
+    expected <- c(
+      "there are 0 apples", "there is one apple",
+      "there are 2 apples", "there are 3 apples"
+    )
+    actual <- nsprintf(0:3, "there is one apple", "there are %d apples")
     n2 <- nsprintf(2, "there is one apples", "there are %d apples")
-    expect_true(identical(n1, "there is one apples"))
-    expect_true(identical(n2, "there are 2 apples"))
+    expect_identical(actual, expected)
+})
+
+test_that("sprintf substitution works", {
+    expected <- c(
+      "Juggling 1 ball is easy", "Juggling 3 balls a good trick",
+      "Juggling 6 balls almost impossible"
+    )
+    actual <- nsprintf(
+      c(1, 3, 6), 
+      "Juggling %d ball is %s", 
+      "Juggling %d balls %s", 
+      c("easy", "a good trick", "almost impossible")
+    )
+    expect_identical(actual, expected)
 })
